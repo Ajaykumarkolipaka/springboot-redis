effa5ecb5bb1aad866dcb633c5daa7b1fb91f271
Ajay <kolipaka02@gmail.com>
2020-01-26 18:59:45 +0530

    adding redis dependencies

--- src/main/java/com/example/springbootredis/config/BeanConfig.java
+++ src/main/java/com/example/springbootredis/config/BeanConfig.java
@@ -1,17 +1,25 @@
 package com.example.springbootredis.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.example.springbootredis.model.Employee;
 
 @Configuration
 public class BeanConfig {
-	
-	/*
-	 * @Bean JedisConnectionFactory getJedisConnectionFactory() { return new
-	 * JedisConnectionFactory(); }
-	 * 
-	 * @Bean RedisTemplate<String, Employee> getRedisTemplate() {
-	 * RedisTemplate<String, Employee> redisTemplate = new RedisTemplate<>();
-	 * redisTemplate.setConnectionFactory(getJedisConnectionFactory()); return
-	 * redisTemplate; }
-	 */
+
+	@Bean
+	JedisConnectionFactory getJedisConnectionFactory() {
+		return new JedisConnectionFactory();
+	}
+
+	@Bean
+	RedisTemplate<String, Employee> getRedisTemplate() {
+		RedisTemplate<String, Employee> redisTemplate = new RedisTemplate<>();
+		redisTemplate.setConnectionFactory(getJedisConnectionFactory());
+		return redisTemplate;
+	}
+
 }
